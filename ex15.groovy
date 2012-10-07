548c1bed7c6aa357fb5944cc7aa2b88336381fe3
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2012-10-07 23:13:57 +0100

    Random number generator substituted in.  Working.

--- ex15.groovy
+++ ex15.groovy
@@ -1,7 +1,7 @@
 println "Try to guess my number!"
 int guess
 int counter = 0
-int numberToGuess = 150 //for testing purposes - will be randomly generated
+int numberToGuess = Math.abs(1000 * Math.random())
 boolean correct = false
 boolean finished = false
 while (!finished) {
