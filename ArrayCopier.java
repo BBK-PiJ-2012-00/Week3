cd42d02deb35823969817cdb8745524585e216e2
Anna Taylor <anna.taylor@dcs-bbk-ac-uk>
2012-10-15 17:23:09 +0100

    Remember simple types are forgotten at the end of method. Sort out first two parts, second (dst = src) needs to copy each value of array, not just pointer, which gets lost at the end.  Heap information must be changed.

--- ArrayCopier.java
+++ ArrayCopier.java
@@ -12,10 +12,8 @@ public class ArrayCopier {
 		secondArray[2] = 5;
 		secondArray[3] = 6;	
 	}
-	public void copy(int[] firstArray, int[] secondArray){
-		int[] src = firstArray;
+	public void copy(int[] src, int[] dst){
 		int srcLength = src.length;
-		int[] dst = secondArray;
 		int dstLength = dst.length;		
 		if(srcLength < dstLength) {
 			int difference = dstLength - srcLength;
@@ -34,11 +32,41 @@ public class ArrayCopier {
 			int counter;
 			int[] srcShortCopy;
 			srcShortCopy = new int[dstLength];
-			for (counter = 0; counter <= dstLength; counter++) {
+			for (counter = 0; counter < dstLength; counter++) {
 				int contents = src[counter];
 				srcShortCopy[counter] = contents;
 			}
-			dst = srcShortCopy;
+			dst = srcShortCopy; //copying pointer only -- lost
 		}
+	}
+	
+	public static void main(String[] args) {
+		int[] firstArray;
+		int[] secondArray;
+		firstArray = new int[7];
+		firstArray[0] = 1;
+		firstArray[1] = 2;
+		firstArray[2] = 2;
+		firstArray[3] = 2;
+		firstArray[4] = 2;
+		firstArray[5] = 2;
+		firstArray[6] = 2;
+		secondArray = new int[4];
+		secondArray[0] = 3;
+		secondArray[1] = 4;
+		secondArray[2] = 5;
+		secondArray[3] = 6;	
+		
+		ArrayCopier copier = new ArrayCopier();
+		
+			copier.copy(firstArray, secondArray);
+			int counter = 0;
+			int secondArrayLength = secondArray.length;
+			while (counter < secondArrayLength) {
+				int i = secondArray[counter];
+				counter++;
+				System.out.println(i);
+			}
+				
 	}
 }
