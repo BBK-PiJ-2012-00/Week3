d06a3a0f2d24d96e473f090cbdcbe2ea10ddf134
Anna Taylor <anna.taylor@dcs-bbk-ac-uk>
2012-10-16 16:35:01 +0100

    Incomplete.

---
+++ notesExercise.java
@@ -0,0 +1,11 @@
+public class EmployeeID {
+
+	private String emp0, emp1, emp2, emp3, emp4, emp5, emp6, emp7, emp8, emp9; 
+	private int id0, id1, id2, id3, id4, id5, id6, id7, id8, id9;
+	
+	Employee(String s, int i) {
+		this.s = s
+		this.i = i
+	}
+	
+	//Groovy - take input, write it to EmployeeID(emp0, id0)
