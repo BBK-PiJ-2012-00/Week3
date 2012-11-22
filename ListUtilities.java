619b654c7f93393f708cd3a2a44e5842bc276f4c
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2012-11-22 10:57:13 +0000

    Contains a method to convert an array to a list.

---
+++ ListUtilities.java
@@ -0,0 +1,22 @@
+public class ListUtilities {
+
+	public static List converter(int[] intArray) {
+		List list = new List();
+		int i;
+		for (i = 0; i < intArray.length; i++) {
+			int num = intArray[i];
+			list.add(num);
+		}
+		return list;
+	}
+	
+}
+	
+	
+
+	
+	
+	
+	
+	
+	
